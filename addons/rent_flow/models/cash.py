f8dd6093761f699b93bc9ac70334a1d850886ca3
BinhTT <binhtt@pptech.com.sg>
2019-11-10 16:56:28 +0800

    Set thời gian cho Cash

--- addons/rent_flow/models/cash.py
+++ addons/rent_flow/models/cash.py
@@ -1,7 +1,7 @@
 # __author__ = 'BinhTT'
 from odoo import models, fields, api, _
 import odoo.addons.decimal_precision as dp
-
+from datetime import date
 
 class RentCash(models.Model):
     _name = 'rent.cash'
@@ -10,12 +10,16 @@ class RentCash(models.Model):
     cash = fields.Float('$', digits=dp.get_precision('Account'),)
     sale_id = fields.Many2one('sale.order')
     reason = fields.Char('Reason')
+    date = fields.Date('Date')
 
     def save_payment(self):
         return {'type': 'ir.actions.act_window_close'}
 
     @api.model
     def create(self, vals):
+        if not vals.get('date', False):
+            vals.update({'date': date.today()})
+
         if vals.get('cash_type') == 'out':
             vals.update({'cash': vals.get('cash', 0) * -1})
         if self._context.get('from_sale', False):
